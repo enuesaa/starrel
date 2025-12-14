9a4e93f1554dd6eca272d55911b5fc9a9b0b33b7
nua <enuesaa@gmail.com>
2025-12-14 23:53:32 +0900

    feat: ページを追加

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router'
+import { About } from './about/about'
+import { Top } from './top/top'
 
-export const routes: Routes = []
+export const routes: Routes = [
+  { path: '', component: Top },
+  { path: 'about', component: About },
+]
