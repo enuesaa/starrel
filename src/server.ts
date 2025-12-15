e8ff55dec966b920ce198dbf9bfb6659d34edbd0
nua <enuesaa@gmail.com>
2025-12-16 00:06:38 +0900

    chore: 不要なコードを削除

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,6 @@
 import {
   AngularNodeAppEngine,
   createNodeRequestHandler,
-  isMainModule,
   writeResponseToNodeResponse,
 } from '@angular/ssr/node'
 import express from 'express'
@@ -12,7 +11,6 @@ const browserDistFolder = join(import.meta.dirname, '../browser')
 const app = express()
 const angularApp = new AngularNodeAppEngine()
 
-// Serve static files from /browser
 app.use(
   express.static(browserDistFolder, {
     maxAge: '1y',
@@ -28,21 +26,6 @@ app.use((req, res, next) => {
     .then((response) => (response ? writeResponseToNodeResponse(response, res) : next()))
     .catch(next)
 })
-
-/**
- * Start the server if this module is the main entry point, or it is ran via PM2.
- * The server listens on the port defined by the `PORT` environment variable, or defaults to 4000.
- */
-if (isMainModule(import.meta.url) || process.env['pm_id']) {
-  const port = process.env['PORT'] || 4000
-  app.listen(port, (error) => {
-    if (error) {
-      throw error
-    }
-
-    console.log(`Node Express server listening on http://localhost:${port}`)
-  })
-}
 
 // Request handler used by the Angular CLI (for dev-server and during build) or Firebase Cloud Functions.
 export const reqHandler = createNodeRequestHandler(app)
