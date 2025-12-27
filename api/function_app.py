9fe6b788f636f94a7947aaf8f270d84624118295
nua <enuesaa@gmail.com>
2025-12-27 19:19:54 +0900

    refactor: define api response schema

--- api/function_app.py
+++ api/function_app.py
@@ -2,7 +2,7 @@ import azure.functions as func
 from ulid import ULID
 from db.repos import upsert_bookmark, list_bookmarks, get_bookmark
 from db.models import Bookmark
-from reqres import schema
+from reqres.schema import ListResponse, ViewResponse, CreateResponse, ErrorResponse
 
 app = func.FunctionApp()
 
@@ -10,35 +10,28 @@ app = func.FunctionApp()
 def handle_list_bookmarks(req: func.HttpRequest) -> func.HttpResponse:
     try:
         bookmarks = list_bookmarks()
-        res = schema.ListResponse(items=bookmarks)
-        return func.HttpResponse(
-            body=res.model_dump_json(),
-            mimetype='application/json',
-            status_code=200
-        )
+        return ListResponse(items=bookmarks).ok()
     except Exception as e:
-        return func.HttpResponse(f'err: {e}', status_code=400)
+        return ErrorResponse(error=e).err400()
 
 @app.route(route='bookmarks/{id}', methods=['GET'], auth_level=func.AuthLevel.ANONYMOUS)
 def handle_get_bookmarks(req: func.HttpRequest) -> func.HttpResponse:
     id = req.route_params.get('id')
     if id is None:
-        return func.HttpResponse('Not Found', status_code=404)
+        return ErrorResponse(error=None).err400()
     try:
         bookmark = get_bookmark(id=id)
         if bookmark is None:
-            return func.HttpResponse('Not Found', status_code=404)
-        res = schema.ViewResponse(data=bookmark)
-        return func.HttpResponse(res.model_dump_json(), mimetype='application/json', status_code=200)
+            return ErrorResponse(error=None).err400()
+        return ViewResponse(data=bookmark).ok()
     except Exception as e:
-        return func.HttpResponse(f'err: {e}', status_code=400)
+        return ErrorResponse(error=e).err400()
 
 @app.route(route='bookmarks', methods=['POST'], auth_level=func.AuthLevel.ANONYMOUS)
 def create_bookmark(req: func.HttpRequest) -> func.HttpResponse:
     try:
         bookmark = Bookmark(id=str(ULID()), type='favorite', url='https://example.com/', title='a', description=None, image=None)
         upsert_bookmark(bookmark=bookmark)
-        res = schema.CreateResponse(ok=True)
-        return func.HttpResponse(res.model_dump_json(), mimetype='application/json', status_code=200)
+        return CreateResponse(success=True).ok()
     except Exception as e:
-        return func.HttpResponse(f'err: {e}', status_code=400)
+        return ErrorResponse(error=e).err400()
--- api/reqres/schema.py
+++ api/reqres/schema.py
@@ -1,12 +1,31 @@
 from pydantic import BaseModel
 from db.models import Bookmark
 from typing import List
+import azure.functions as func
 
 class ListResponse(BaseModel):
     items: List[Bookmark]
 
+    def ok(self) -> func.HttpResponse:
+        return func.HttpResponse(self.model_dump_json(), mimetype='application/json', status_code=200)
+
 class ViewResponse(BaseModel):
     data: Bookmark
 
+    def ok(self) -> func.HttpResponse:
+        return func.HttpResponse(self.model_dump_json(), mimetype='application/json', status_code=200)
+
 class CreateResponse(BaseModel):
-    ok: bool
+    success: bool
+
+    def ok(self) -> func.HttpResponse:
+        return func.HttpResponse(self.model_dump_json(), mimetype='application/json', status_code=200)
+
+class ErrorResponse(BaseModel):
+    error: Exception|None
+
+    def err400(self) -> func.HttpResponse:
+        return func.HttpResponse(self.model_dump_json(), mimetype='application/json', status_code=400)
+    
+    def err404(self) -> func.HttpResponse:
+        return func.HttpResponse('{}', mimetype='application/json', status_code=404)
