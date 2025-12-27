a76a4a07b44a279f057bb7caafdc48c7450e2dcd
nua <enuesaa@gmail.com>
2025-12-27 16:19:06 +0900

    chore: remove about page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,6 @@
 import { Routes } from '@angular/router'
-import { About } from './pages/about/about'
 import { Top } from './pages/top/top'
 
 export const routes: Routes = [
   { path: '', component: Top },
-  { path: 'about', component: About },
 ]
--- src/app/pages/about/about.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core'
-import { Dashboard } from '../../components/dashboard/dashboard'
-
-@Component({
-  imports: [Dashboard],
-  templateUrl: './about.html',
-})
-export class About {}
