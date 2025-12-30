226f9cc2ddfb176ed4b438d3ec5080a03c89a399
nua <enuesaa@gmail.com>
2025-12-30 23:58:32 +0900

    fix: move auth config to app config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,20 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core'
 import { provideRouter, withHashLocation } from '@angular/router'
+import { provideAuth0 } from '@auth0/auth0-angular'
+import { environment } from '../environments/environment'
 
 import { routes } from './app.routes'
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideBrowserGlobalErrorListeners(), provideRouter(routes, withHashLocation())],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(routes, withHashLocation()),
+    provideAuth0({
+      domain: environment.authDomain,
+      clientId: environment.authClientId,
+      authorizationParams: {
+        redirect_uri: window.location.href,
+      },
+    }),
+  ],
 }
--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,5 @@
 import { bootstrapApplication } from '@angular/platform-browser'
 import { appConfig } from './app/app.config'
 import { AppComponent } from './app/app.component'
-import { provideAuth0 } from '@auth0/auth0-angular'
-import { mergeApplicationConfig } from '@angular/core'
-import { environment } from './environments/environment'
 
-const authConfig = mergeApplicationConfig(appConfig, {
-  providers: [
-    provideAuth0({
-      domain: environment.authDomain,
-      clientId: environment.authClientId,
-      authorizationParams: {
-        redirect_uri: window.location.href,
-      },
-    }),
-  ],
-})
-
-bootstrapApplication(AppComponent, authConfig).catch((err) => console.error(err))
+bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err))
