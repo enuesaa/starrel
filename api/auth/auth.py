d16a6afe7947ff34e7fd273de6d60ee58949a65e
nua <enuesaa@gmail.com>
2025-12-30 16:24:43 +0900

    feat: asyncio の呼び出しに問題がないことを確認した

--- api/auth/auth.py
+++ api/auth/auth.py
@@ -5,12 +5,19 @@ import asyncio
 auth_domain = os.environ["AUTH_DOMAIN"]
 auth_audience = os.environ["AUTH_AUDIENCE"]
 
-async def verify_jwt(token: str):
+# async def verify_jwt(token: str):
+#     client = ApiClient(ApiClientOptions(
+#         domain=auth_domain,
+#         audience=auth_audience,
+#     ))
+#     return await client.verify_access_token(access_token=token)
+
+async def aa():
+    return "aa"
+
+def verify(token: str):
     client = ApiClient(ApiClientOptions(
         domain=auth_domain,
         audience=auth_audience,
     ))
-    return await client.verify_access_token(access_token=token)
-
-def verify(token: str):
-    return asyncio.run(verify_jwt(token=token))
+    return asyncio.run(aa())
