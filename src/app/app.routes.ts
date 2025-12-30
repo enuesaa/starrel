1a92fafdc9d78c5b68a0b2654fc6531f79f0b970
nua <enuesaa@gmail.com>
2025-12-31 00:42:00 +0900

    chore: mark cursor-pointer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,13 +3,11 @@ import { Top } from './pages/top/top'
 import { DomainListComponent } from './pages/domain-list/domain-list.component'
 import { SearchComponent } from './pages/search/search.component'
 import { LoginComponent } from './pages/login/login.component'
-import { ProfileComponent } from './pages/profile/profile.component'
 import { authGuard } from './guards/auth.guard'
 
 export const routes: Routes = [
   { path: '', component: Top, canActivate: [authGuard] },
   { path: 'login', component: LoginComponent },
-  { path: 'profile', component: ProfileComponent },
   { path: 'domain/:domain', component: DomainListComponent, canActivate: [authGuard] },
   { path: 'search', component: SearchComponent, canActivate: [authGuard] },
 ]
