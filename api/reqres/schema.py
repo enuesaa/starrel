bcf1fe8e6405e1ea35eb34c2596e13847cc2f799
nua <enuesaa@gmail.com>
2026-01-02 19:42:27 +0900

    feat: cors header は azure functions レベルで付与する

--- api/reqres/schema.py
+++ api/reqres/schema.py
@@ -3,30 +3,23 @@ from db.models import Bookmark
 from typing import List
 import azure.functions as func
 
-cors_headers = {
-    'Access-Control-Allow-Origin': 'http://localhost:3000',
-    'Access-Control-Allow-Methods': '*',
-    'Access-Control-Allow-Headers': '*',
-    'Access-Control-Allow-Credentials': 'true',
-}
-
 class ListResponse(BaseModel):
     items: List[Bookmark]
 
     def ok(self) -> func.HttpResponse:
-        return func.HttpResponse(self.model_dump_json(), mimetype='application/json', status_code=200, headers=cors_headers)
+        return func.HttpResponse(self.model_dump_json(), mimetype='application/json', status_code=200)
 
 class ViewResponse(BaseModel):
     data: Bookmark
 
     def ok(self) -> func.HttpResponse:
-        return func.HttpResponse(self.model_dump_json(), mimetype='application/json', status_code=200, headers=cors_headers)
+        return func.HttpResponse(self.model_dump_json(), mimetype='application/json', status_code=200)
 
 class CreateResponse(BaseModel):
     success: bool
 
     def ok(self) -> func.HttpResponse:
-        return func.HttpResponse(self.model_dump_json(), mimetype='application/json', status_code=200, headers=cors_headers)
+        return func.HttpResponse(self.model_dump_json(), mimetype='application/json', status_code=200)
 
 class ErrorResponse(BaseModel):
     model_config = ConfigDict(arbitrary_types_allowed=True)
@@ -37,14 +30,14 @@ class ErrorResponse(BaseModel):
         return str(e) if e else None
 
     def err400(self) -> func.HttpResponse:
-        return func.HttpResponse(self.model_dump_json(), mimetype='application/json', status_code=400, headers=cors_headers)
+        return func.HttpResponse(self.model_dump_json(), mimetype='application/json', status_code=400)
     
     def err403(self) -> func.HttpResponse:
-        return func.HttpResponse(self.model_dump_json(), mimetype='application/json', status_code=403, headers=cors_headers)
+        return func.HttpResponse(self.model_dump_json(), mimetype='application/json', status_code=403)
 
     def err404(self) -> func.HttpResponse:
-        return func.HttpResponse('{}', mimetype='application/json', status_code=404, headers=cors_headers)
+        return func.HttpResponse('{}', mimetype='application/json', status_code=404)
 
 class CorsResponse(BaseModel):
     def ok(self) -> func.HttpResponse:
-        return func.HttpResponse('', status_code=200, headers=cors_headers)
+        return func.HttpResponse('', status_code=200)
