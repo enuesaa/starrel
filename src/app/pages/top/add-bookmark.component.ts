9f80293fa8e962d82bffae35bb37fc10c93c570b
nua <enuesaa@gmail.com>
2026-01-03 14:48:13 +0900

    feat: モバイルでロード時にクリップボードへアクセスできないとわかったので、ペーストボタンを置いてみる

--- src/app/pages/top/add-bookmark.component.ts
+++ src/app/pages/top/add-bookmark.component.ts
@@ -1,4 +1,4 @@
-import { Component, signal, output, OnInit } from '@angular/core'
+import { Component, signal, output } from '@angular/core'
 import { FormsModule } from '@angular/forms'
 
 @Component({
@@ -7,20 +7,17 @@ import { FormsModule } from '@angular/forms'
   imports: [FormsModule],
   templateUrl: './add-bookmark.component.html',
 })
-export class AddBookmarkComponent implements OnInit {
+export class AddBookmarkComponent {
   urlInput = signal('')
   addBookmark = output<string>()
 
-  ngOnInit() {
-    this.checkClipboard()
-  }
 
-  async checkClipboard() {
+  async onPasteClick() {
     try {
       const text = await navigator.clipboard.readText()
       new URL(text) // Validate URL
       this.urlInput.set(text)
-    } catch {}
+    } catch { }
   }
 
   onAddClick() {
